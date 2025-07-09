52b7a71b5277ad264a609e3ecc4fb1968a9a835c
Akiya <115332701+agarner2115@users.noreply.github.com>
2025-07-09 01:36:28 -0400

    Update lora_transmitter.py

--- lora_transmitter.py
+++ lora_transmitter.py
@@ -12,15 +12,20 @@ lora = serial.Serial(port='/dev/ttyS0', baudrate=9600, parity=serial.PARITY_NONE
 # lora = serial.Serial(port='/dev/ttyAMA0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
 def loraTX_running():
-    while True:
-        if not data_queue.empty():
-            #Get weather data from the BME280 sensor
-            temperature, pressure, humidity = read_bme280()  # Assuming this function returns temperature, pressure, and humidity
-            altitude = calculate_altitude(pressure)  #Calculate altitude based on pressure
-            #Format the data as a string
-            data = f"Temperature: {temperature}°C, Pressure: {pressure} hPa, Humidity: {humidity}%, Altitude: {altitude} m"
-            b = bytes(data, 'utf-8')  #Convert string into bytes
-            lora.write(b)  #Send the data to the other LoRa
-            print(f"Sent: {data}")
-        
-        time.sleep(0.2)  # Delay of 200ms
+    try:
+        while not stop_event.is_set():
+                if not data_queue.empty():
+                    data = data_queue.get()
+                #Get weather data from the BME280 sensor
+                temperature, pressure, humidity = read_bme280()  # Assuming this function returns temperature, pressure, and humidity
+                altitude = calculate_altitude(pressure)  #Calculate altitude based on pressure
+                #Format the data as a string
+                data = f"Temperature: {temperature}°C, Pressure: {pressure} hPa, Humidity: {humidity}%, Altitude: {altitude} m"
+                b = bytes(data, 'utf-8')  #Convert string into bytes
+                lora.write(b)  #Send the data to the other LoRa
+                print(f"Sent: {data}")
+            
+            time.sleep(0.2)  # Delay of 200ms
+
+except Exception as e:
+        print(f"[LoRa TX] Error: {e}")
