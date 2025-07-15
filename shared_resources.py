caf71063fcf91902ffe755056ee1e1a55da3183c
Akiya <115332701+agarner2115@users.noreply.github.com>
2025-07-15 05:00:07 +0100

    Update shared_resources.py

--- shared_resources.py
+++ shared_resources.py
@@ -1,6 +1,7 @@
 #This module is used for accessing shared resources in the WES project.
-
 import queue
+import threading
 
 # Shared data queue
 data_queue = queue.Queue()
+stop_event = threading.Event()
